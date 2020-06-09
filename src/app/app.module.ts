70e1ca2d5160ba221b9217b4198cfec5c42d2f6f
ishara <ishara.18@itfac.mrt.ac.lk>
2020-06-09 09:04:11 -0700

    contact page

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { ReactiveFormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactComponent,
+ 
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatCardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
